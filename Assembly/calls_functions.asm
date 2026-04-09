21076189c699e88807184f006446d70d96d1c76b
tobias-barakaa <ngairato@gmail.com>
2026-04-09 14:31:56 +0300

    programm to guess age in 10years project

--- Assembly/calls_functions.asm
+++ Assembly/calls_functions.asm
@@ -2,3 +2,31 @@
 
 BITS 64
 default rel 
+
+extern printf 
+global main 
+
+section .data 
+fmt db "%ld", 10, 0
+
+section .text 
+
+_sum:
+; EXECUTES HERE
+    add rdi, rsi
+    mov rax, rdi
+    ret
+
+main: 
+    mov rdi, 50
+    mov rsi, 20
+    call _sum
+    ; ADDRES HERE , GOES TO STACK
+    ; print the result
+    mov rdi, fmt 
+    mov rsi, rax 
+    xor eax, eax 
+    call printf 
+
+    xor eax, eax
+    ret
